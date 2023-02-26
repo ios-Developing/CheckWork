4ae2d5f2df3822486208634104305dcc08b8c82d
Dmitry <station@MacBook-Air-Dmitry.local>
2023-02-26 10:20:47 +0300

    HomeWork 5

--- Program.cs
+++ Program.cs
@@ -1,45 +1,75 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
 
-Console.Write("----Задача 25----\n");
-int A, B, C;
-Console.Write("Enter number A: ");
-int.TryParse(Console.ReadLine()!, out A);
-C = A;
-Console.Write("Enter number B: ");
-int.TryParse(Console.ReadLine()!, out B);
-for (int n = 1; n < B; n++)
-    C = C * A;
-Console.Write($"А в степени В = {C} ");
+Console.Write("----Задача 34----\n");
+int[] array = FillArray(10, 100, 999);
+int evenNumber = array.Length;
+Console.Write($"{String.Join(" , ", array) }\n");
 
-// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
-
-Console.Write("----Задача 27----\n");
-int A1, summ = 0;
-Console.Write("Enter number A: ");
-int.TryParse(Console.ReadLine()!, out A1);
-while (A1 != 0)
+foreach (int each in array) 
 {
-    summ = summ + A1 % 10;
-    A1 = A1 / 10;
+    evenNumber = evenNumber - each % 2; // расчет чётных чисел в массиве.
 }
-Console.Write($"Summ = {A1} {summ} \n");
+Console.Write($"Kоличество чётных чисел в массиве: {evenNumber}\n\n"); //вывод кол-ва чётных чисел в массиве.
 
-//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33 -> [6, 1, 33]
-
-Console.Write("----Задача 29----\n");
-int[] arr;
-arr = new int[8];
-for (int index = 0; index <= 7; index++)
+int[] FillArray(int size, int min = 100, int max = 999) //заполнение случайными положительными трёхзначными числами
 {
-    Console.Write($"Enter number {index} to add Array: ");
-    int.TryParse(Console.ReadLine()!, out arr[index]);
-    Console.Write($"Array[{index}] = {arr[index]} \n");
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min, max);
+    return arr;
 }
-Array.ForEach(arr, Console.WriteLine); // printing all contents of array in C#
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Write("----Задача 36----\n");
+int[] array2 = FillArray1(6, -20, 20);
+int oddSumm = 0;                                        // объявление переменной сумма элементов, стоящих на нечётных позициях.
+Console.Write($"{String.Join(" , ", array2) }\n");
+
+for (int index = 1; index < array2.Length; index += 2)
+    oddSumm = oddSumm + array2[index];
+
+Console.Write($"Cумма элементов, стоящих на нечётных позициях: {oddSumm}\n\n"); //вывод кол-ва чётных чисел в массиве.
+
+int[] FillArray1(int size, int min = 100, int max = 999) //заполнение случайными положительными трёхзначными числами
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min, max);
+    return arr;
+}
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Write("----Задача 38----\n");
+int[] array3 = FillArray2(10, 20, 200);
+int max, min, diff;                                        // объявление переменных
+Console.Write($"{String.Join(" , ", array3) }\n");
+
+max = array3[0];
+min = array3[0];
+
+foreach(int each in array3)
+{
+    if (each > max)
+        max = each;
+    else if (each < min)
+        min = each;
+}
+diff = max - min;   //Найдите разницу между максимальным и минимальным элементов массива
+
+Console.Write($"Max: {max}\nMin: {min}\nPазницa между максимальным и минимальным элементов массива: {diff}\n"); //вывод чисел
+
+int[] FillArray2(int size, int min = 100, int max = 999) //заполнение случайными положительными трёхзначными числами
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(min, max);
+    return arr;
+}
