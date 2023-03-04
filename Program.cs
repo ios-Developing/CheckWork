ab8abda5b034a5621eb2129b9177691c58fbe84e
Dmitry <station@MacBook-Air-Dmitry.local>
2023-03-04 11:29:24 +0300

    HomeWork 6

--- Program.cs
+++ Program.cs
@@ -1,75 +1,61 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
-// Напишите программу, которая покажет количество чётных чисел в массиве.
-// [345, 897, 568, 234] -> 2
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
 
-Console.Write("----Задача 34----\n");
-int[] array = FillArray(10, 100, 999);
-int evenNumber = array.Length;
-Console.Write($"{String.Join(" , ", array) }\n");
+int m_number;
+int countMoreThanO = 0;
+Input("Сколько чисел будем вводить? ");
+int count = m_number;                           //сохраним сколько чисел будем вводить
+int[] array_numbers = new int[m_number];
 
-foreach (int each in array) 
-{
-    evenNumber = evenNumber - each % 2; // расчет чётных чисел в массиве.
+for(int i = 0; i < count; i++){                 //цикл ввода чисел в массив
+    Input($"Please enter number {i + 1}: ");
+    array_numbers[i] = m_number; 
+    if (m_number>0)
+        countMoreThanO++;
 }
-Console.Write($"Kоличество чётных чисел в массиве: {evenNumber}\n\n"); //вывод кол-ва чётных чисел в массиве.
+//Console.Write($"Чисел больше 0: {countMoreThanO}\n");
+//Console.Write($"Проверьте сами: {String.Join(" , ", array_numbers) }\n");  // printing all contents of array in C# через запятую
 
-int[] FillArray(int size, int min = 100, int max = 999) //заполнение случайными положительными трёхзначными числами
+Print("Чисел больше 0:", countMoreThanO);
+Console.Write($"Проверьте сами: {String.Join(" , ", array_numbers) }\n");  // printing all contents of array in C# через запятую
+
+void Print(string some_text, int result) //Метод вывода
 {
-    int[] arr = new int[size];
-    for (int i = 0; i < size; i++)
-        arr[i] = new Random().Next(min, max);
-    return arr;
-}
-
-// Задача 36: Задайте одномерный массив, заполненный случайными числами.
-// Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
-
-Console.Write("----Задача 36----\n");
-int[] array2 = FillArray1(6, -20, 20);
-int oddSumm = 0;                                        // объявление переменной сумма элементов, стоящих на нечётных позициях.
-Console.Write($"{String.Join(" , ", array2) }\n");
-
-for (int index = 1; index < array2.Length; index += 2)
-    oddSumm = oddSumm + array2[index];
-
-Console.Write($"Cумма элементов, стоящих на нечётных позициях: {oddSumm}\n\n"); //вывод кол-ва чётных чисел в массиве.
-
-int[] FillArray1(int size, int min = 100, int max = 999) //заполнение случайными положительными трёхзначными числами
-{
-    int[] arr = new int[size];
-    for (int i = 0; i < size; i++)
-        arr[i] = new Random().Next(min, max);
-    return arr;
+    Console.Write($"{some_text} {result}\n");
 }
 
-// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
-// [3 7 22 2 78] -> 76
-
-Console.Write("----Задача 38----\n");
-int[] array3 = FillArray2(10, 20, 200);
-int max, min, diff;                                        // объявление переменных
-Console.Write($"{String.Join(" , ", array3) }\n");
-
-max = array3[0];
-min = array3[0];
-
-foreach(int each in array3)
+int Input(string some_text) //Метод ввода чисел
 {
-    if (each > max)
-        max = each;
-    else if (each < min)
-        min = each;
+    Console.Write($"{some_text}");
+    int.TryParse(Console.ReadLine()!, out m_number);
+    return m_number;
 }
-diff = max - min;   //Найдите разницу между максимальным и минимальным элементов массива
 
-Console.Write($"Max: {max}\nMin: {min}\nPазницa между максимальным и минимальным элементов массива: {diff}\n"); //вывод чисел
 
-int[] FillArray2(int size, int min = 100, int max = 999) //заполнение случайными положительными трёхзначными числами
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int b1 = Input1("Input b1: ");
+int k1 = Input1("Input k1: ");
+int b2 = Input1("Input b2: ");
+int k2 = Input1("Input k2: ");
+
+CrossPoint(k1,b1,k2,b2);
+
+void CrossPoint(double k1, double b1, double k2, double b2)
 {
-    int[] arr = new int[size];
-    for (int i = 0; i < size; i++)
-        arr[i] = new Random().Next(min, max);
-    return arr;
+double x = Math.Round((b1-b2)/(k2-k1), 2);
+double y = Math.Round((k2*b1-k1*b2)/(k2-k1), 2);
+if(k1==k2) Console.Write("Заданные прямые не пересекаются!"); else
+    Console.Write($"Точка пересечения заданных прямых: ({x}; {y}) ");
+}
+
+int Input1(string some_text) //Метод ввода чисел
+{
+    int _n;
+    Console.Write($"{some_text}");
+    int.TryParse(Console.ReadLine()!, out _n);
+    return _n;
 }
