5c5ed07b63601547aab6f97c415880648e9db90a
Dmitry <station@MacBook-Air-Dmitry.local>
2023-03-04 17:56:31 +0300

    HomeWork 7

--- Program.cs
+++ Program.cs
@@ -1,61 +1,59 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
 
-int m_number;
-int countMoreThanO = 0;
-Input("Сколько чисел будем вводить? ");
-int count = m_number;                           //сохраним сколько чисел будем вводить
-int[] array_numbers = new int[m_number];
-
-for(int i = 0; i < count; i++){                 //цикл ввода чисел в массив
-    Input($"Please enter number {i + 1}: ");
-    array_numbers[i] = m_number; 
-    if (m_number>0)
-        countMoreThanO++;
-}
-//Console.Write($"Чисел больше 0: {countMoreThanO}\n");
-//Console.Write($"Проверьте сами: {String.Join(" , ", array_numbers) }\n");  // printing all contents of array in C# через запятую
-
-Print("Чисел больше 0:", countMoreThanO);
-Console.Write($"Проверьте сами: {String.Join(" , ", array_numbers) }\n");  // printing all contents of array in C# через запятую
-
-void Print(string some_text, int result) //Метод вывода
+Console.Clear();
+Console.Write("----Задача 47----\n");
+int [,] arr = new int[3, 4];
+for (int i = 0; i < arr.GetLength(0); i++)
 {
-    Console.Write($"{some_text} {result}\n");
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().Next(100);
+          Console.Write($"{arr[i, j]} ");
+    }
+    Console.WriteLine();
 }
+Console.WriteLine();
 
-int Input(string some_text) //Метод ввода чисел
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Write("----Задача 50----\n");
+int i1, j1;
+Console.Write("Enter row index: ");
+int.TryParse(Console.ReadLine()!, out i1);
+Console.Write("Enter column index: ");
+int.TryParse(Console.ReadLine()!, out j1);
+
+if (i1 > arr.GetLength(0) || j1 > arr.GetLength(1))
+    Console.Write($"Value arr[{i1}, {j1}] Not exist\n\n");
+    else
+        Console.Write($"{arr[i1-1, j1-1]} \n\n");
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.ф
+
+Console.Write("----Задача 52----\n");
+for (int j2 = 0; j2 < arr.GetLength(1); j2++)
 {
-    Console.Write($"{some_text}");
-    int.TryParse(Console.ReadLine()!, out m_number);
-    return m_number;
-}
-
-
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
-//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
-int b1 = Input1("Input b1: ");
-int k1 = Input1("Input k1: ");
-int b2 = Input1("Input b2: ");
-int k2 = Input1("Input k2: ");
-
-CrossPoint(k1,b1,k2,b2);
-
-void CrossPoint(double k1, double b1, double k2, double b2)
-{
-double x = Math.Round((b1-b2)/(k2-k1), 2);
-double y = Math.Round((k2*b1-k1*b2)/(k2-k1), 2);
-if(k1==k2) Console.Write("Заданные прямые не пересекаются!"); else
-    Console.Write($"Точка пересечения заданных прямых: ({x}; {y}) ");
-}
-
-int Input1(string some_text) //Метод ввода чисел
-{
-    int _n;
-    Console.Write($"{some_text}");
-    int.TryParse(Console.ReadLine()!, out _n);
-    return _n;
+    int summ = 0;
+    double average = 0;
+    for (int i2 = 0; i2 < arr.GetLength(0); i2++)
+    {
+        summ = summ + arr[i2, j2];  
+    }
+    average = Math.Round((double) summ / arr.GetLength(0), 2);
+    Console.Write($"Average column {j2+1}: {average} ");
+    Console.WriteLine();
 }
