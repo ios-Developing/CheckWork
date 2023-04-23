c4270c661b50ca327b6ad668f5fa4416c6385314
Dmitry <station@MacBook-Air-Dmitry.local>
2023-04-23 17:42:34 +0300

    Add Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// New file
+
+
+Console.Write("----Check Work----\n");
+string[] arrString;
+//arrString = new string[3];
+arrString = new string[] {"мама","мыла","раму", "а","папа","не","мыл","раму","кот","сидит","у","кит","рот", "ток", "мышка", "а"};
+// Console.Write($"{arrString[0]} \n");
+for (int i = 0; i < arrString.Length; i++)
+{
+    if (arrString[i].Length >= 3)
+        Console.Write($"{arrString[i]}, ");    
+}
+Console.WriteLine("...The End!");
